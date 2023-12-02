f1e4cea8b761a03096055eea46f6312ef4169796
d <gitdavit@mail.ru>
2023-12-02 19:44:25 +0400

    weather-app

---
+++ weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "061150c722msh8f609c3af4c22c3p1c8c9ejsn447eb7ba0e09",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/`;
+export const WEATHER_API_KEY = "90f5128b1f9efa9329dabaf033032059";
